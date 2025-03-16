6e49729f3db714692bbc9904f93a02b4a3f1ba1f
Leandro Atero <leandro.atero@innovex.cl>
2025-03-16 18:50:08 -0300

    Update keymaps for sofle

---
+++ keyboards/sofle/keymaps/si/config.h
@@ -0,0 +1,26 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
+// Configuración de RGB
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP 10
+#    define RGBLIGHT_SAT_STEP 17
+#    define RGBLIGHT_VAL_STEP 17
+#endif
+
---
+++ keyboards/sofle/keymaps/si/rules.mk
@@ -0,0 +1,3 @@
+RGBLIGHT_ENABLE = yes    # Activar LEDs RGB
+WPM_ENABLE = yes         # Opcional: para mostrar WPM en la pantalla
+TRI_LAYER_ENABLE = yes
